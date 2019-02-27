6ca0b305a1afb7b08c1bf256ae8b4b612a21aadf
MrBrokkoli <46528044+MrBrokkoli@users.noreply.github.com>
2019-02-27 16:59:07 +0300

    Create python2.py

---
+++ python2.py
@@ -0,0 +1,20 @@
+import string
+import random
+import sys
+
+def generator_function(n, k):
+    for _ in range(n):
+        yield ''.join(random.choices(string.ascii_letters, k=k))
+        
+N = 5
+list1 = [''.join(random.choices(string.ascii_letters, k=N)) for _ in range(1000)]
+list2 = list()
+#list2 = generator_function(1000, 5)
+
+for i in generator_function(1000, 5):
+    list2.append(i)
+
+#for i in list2:
+#    print(i)
+print(list1[0], list1[1], list2[0], list2[1])
+print(sys.getsizeof(list1), sys.getsizeof(generator_function))
