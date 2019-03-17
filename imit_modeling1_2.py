d2bcc403d0826cfc61e5136a00fae43358c2470c
MrBrokkoli <46528044+MrBrokkoli@users.noreply.github.com>
2019-03-17 12:18:19 +0300

    Update imit_modeling1_2.py

--- imit_modeling1_2.py
+++ imit_modeling1_2.py
@@ -32,15 +32,20 @@ j = 2
 np.corrcoef(Z[i - 1], Z[j - 1])[1, 0] # проверка гипотезы равенства 
 
 #https://en.wikipedia.org/wiki/Fisher_transformation
+# sample correlation coefficient
 r = np.corrcoef(Z[i], Z[j])[1,0] # эмпирический коэффицент корреляции
 print("r =", r)
+# population correlation coefficient
 p = G[i][j] / math.sqrt(G[i][i]*G[j][j])
 print("p =", p)
 
+# Fisher's z-transformation of r
 zr = np.arctanh(r)
+# Fisher's z-transformation of p
 zp = np.arctanh(p)
 
 #mean = math.log((1 + p) / (1 - p)) / 2
+# standard error
 std = 1 / math.sqrt(len(Z[i]) - 3)
 
 # https://www.youtube.com/watch?v=R2bGZreVtDc
